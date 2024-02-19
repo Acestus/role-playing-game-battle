d55d6d01cd118d8e46408ae0c01911cb5d3e40d6
wbalconi <wbalconi@3arrows.us>
2024-02-19 16:52:20 -0500

    Daily Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int heroHealth = 10;
+int montsterHealth = 10;
+Random random = new Random();
+int heroAttack = random.Next(1, 11);
+int monsterAttack = random.Next(1, 11);
+
+while (heroHealth > 0 && montsterHealth > 0)
+{
+    Console.WriteLine("Hero attacks the monster for " + heroAttack + " damage!");
+    montsterHealth -= heroAttack;
+    Console.WriteLine("Monster has " + montsterHealth + " health left!");
+    if (montsterHealth <= 0) continue;
+    Console.WriteLine("Monster attacks the hero for " + monsterAttack + " damage!");
+    heroHealth -= monsterAttack;
+    Console.WriteLine("Hero has " + heroHealth + " health left!");
+}
+if (heroHealth <= 0)
+{
+    Console.WriteLine("Hero has been defeated!");
+}
+else
+{
+    Console.WriteLine("Monster has been defeated!");
+}
--- obj/Debug/net8.0/role-playing-game-battle.AssemblyInfo.cs
+++ obj/Debug/net8.0/role-playing-game-battle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("role-playing-game-battle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68dcbbf047d471938a5153c0eba462deb90f849e")]
 [assembly: System.Reflection.AssemblyProductAttribute("role-playing-game-battle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("role-playing-game-battle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
